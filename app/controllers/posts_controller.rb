87314c2ab2f699c4a5f7de6b847e21c387b949e2
helmihidzir <helmihidzir@gmail.com>
2019-07-25 18:59:01 +0800

    Fix minor issue

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :find_post, only: [:show, :edit, :update, :destroy]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
 
   def index

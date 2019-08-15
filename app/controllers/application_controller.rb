84a489d1306c32fc38cb4cc924fec7564c0aa6a7
helmihidzir <helmihidzir@gmail.com>
2019-08-15 15:47:59 +0800

    Edit notification to only increase when incoming friend request is sent

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   helper_method :notification
 
   def notification
-    @total_request = (FriendRequest.where(friend: current_user) + current_user.friend_requests).count
+    @total_request = (FriendRequest.where(friend: current_user)).count
   end
 end

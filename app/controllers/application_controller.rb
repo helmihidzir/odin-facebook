79c10492252bf748fcc8731531595ffa49c0a54b
helmihidzir <helmihidzir@gmail.com>
2019-08-15 15:36:45 +0800

    Add counter to notification

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  helper_method :notification
+
+  def notification
+    @total_request = (FriendRequest.where(friend: current_user) + current_user.friend_requests).count
+  end
 end

f080661425173b9389bbc701088a24fb01602e86
helmihidzir <helmihidzir@gmail.com>
2019-08-19 15:47:12 +0800

    Add name to user model and devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   helper_method :notification
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   # to allow access to all pundit method
   include Pundit
@@ -15,5 +16,13 @@ class ApplicationController < ActionController::Base
   def current_user_not_authorized
     flash[:alert] = 'You do are not authorized for this action.'
     redirect_to root_url
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password)}
+
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password)}
   end
 end
